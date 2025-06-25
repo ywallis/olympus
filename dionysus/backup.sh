805dbddadbe74e50cc41c8aaaddf41a98f0de2f8
ywallis <yann.wallis@gmail.com>
2025-06-25 17:11:35 +0200

    Cleaned up script

--- dionysus/backup.sh
+++ dionysus/backup.sh
@@ -3,7 +3,10 @@
 SOURCE="/home/$USER/dionysus"
 DESTINATION="rsync@hera.olympus:/volume1/Backup/"
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
-LOGFILE="~/logs/daily_backup.log"
+LOGDIR="/home/$USER/logs"
+LOGFILE="$LOGDIR/daily_backup.log"
 
+mkdir -p "$LOGDIR"
+echo "Backup started at $DATE" >> "$LOGFILE"
 rsync -az --delete "$SOURCE" "$DESTINATION" >> "$LOGFILE" 2>&1
 echo "Backup completed at $DATE" >> "$LOGFILE"
