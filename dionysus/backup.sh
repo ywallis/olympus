6ca1a234499b57c44a116389132d30003f5377f4
ywallis <yann.wallis@gmail.com>
2025-06-25 16:42:48 +0200

    Added backup script

---
+++ dionysus/backup.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+SOURCE="/home/$USER/dionysus"
+DESTINATION="rsync@hera.olympus:/volume1/Backup/"
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+LOGFILE="/var/log/daily_backup.log"
+
+rsync -az --delete "$SOURCE" "$DESTINATION" >> "$LOGFILE" 2>&1
+echo "Backup completed at $DATE" >> "$LOGFILE"
