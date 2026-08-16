b168aea11f60b291bfa51be9419af953dbc37da5
Yann Wallis <yann.wallis@gmail.com>
2026-08-16 14:42:01 +0200

    introducing bookorbit

--- dionysus/backup.sh
+++ dionysus/backup.sh
@@ -1,8 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
 USER=$(whoami)
 SOURCE="/home/$USER/dionysus"
 DESTINATION="rsync@hera.olympus:/volume1/Backup/"
+ENV_SOURCE="/home/$USER/olympus/dionysus/.env"
+ENV_DESTINATION="rsync@hera.olympus:/volume1/Backup/dionysus/"
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
 LOGDIR="/home/$USER/logs"
 LOGFILE="$LOGDIR/daily_backup.log"
@@ -10,4 +12,5 @@ LOGFILE="$LOGDIR/daily_backup.log"
 mkdir -p "$LOGDIR"
 echo "Backup started at $DATE" >> "$LOGFILE"
 rsync -az --delete "$SOURCE" "$DESTINATION" >> "$LOGFILE" 2>&1
+rsync -az "$ENV_SOURCE" "$ENV_DESTINATION" >> "$LOGFILE" 2>&1
 echo "Backup completed at $DATE" >> "$LOGFILE"
--- dionysus/restore.sh
+++ dionysus/restore.sh
@@ -3,6 +3,8 @@
 USER=$(whoami)
 SOURCE="rsync@hera.olympus:/volume1/Backup/dionysus/"
 DESTINATION="/home/$USER/dionysus/"
+ENV_SOURCE="rsync@hera.olympus:/volume1/Backup/dionysus/.env"
+ENV_DESTINATION="/home/$USER/olympus/dionysus/"
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
 LOGDIR="/home/$USER/logs"
 LOGFILE="$LOGDIR/daily_import.log"
@@ -10,4 +12,5 @@ LOGFILE="$LOGDIR/daily_import.log"
 mkdir -p "$LOGDIR"
 echo "Import started at $DATE" >> "$LOGFILE"
 rsync -az "$SOURCE" "$DESTINATION" >> "$LOGFILE" 2>&1
+rsync -az "$ENV_SOURCE" "$ENV_DESTINATION" >> "$LOGFILE" 2>&1
 echo "Import completed at $DATE" >> "$LOGFILE"
